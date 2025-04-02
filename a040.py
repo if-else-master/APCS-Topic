737b615773ed683224a07259e5141e3a6615e8de
Raymond <78069313+if-else-master@users.noreply.github.com>
2025-04-02 09:48:34 +0800

    update

--- a040.py
+++ a040.py
@@ -1,1 +1,37 @@
-print('hello world')
+try:
+    num1 = input().split()
+
+    int_num1 = int(num1[0])
+    int_num2 = int(num1[1])
+    int_long = []
+
+    armstrong_list = []
+
+    n = 0
+
+    for i in range(int_num1, int_num2+1):
+        n = 0
+        str_num = str(i)
+        num_long = len(str_num)  # 數字的長度
+        
+       
+        
+
+        one_num = list(str(i))  # 將數字拆解成 ['1', '2', '3']
+        int_one = list(map(int,one_num))
+
+        
+
+        result = sum(x**num_long for x in int_one)
+        aa = int(i)       
+        if result == aa:
+            print(str_num)
+            n +=1
+            
+
+    if n == 0:
+        print("none")
+        
+
+except Exception as e:
+    print('發生錯誤:', e)
