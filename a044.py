9af939c6652d59e312a3e531c14f2b4ca69eb808
Raymond <78069313+if-else-master@users.noreply.github.com>
2025-04-05 12:21:14 +0800

    update

--- a044.py
+++ a044.py
@@ -1,9 +1,14 @@
 while True:
     try:
         num = int(input())
-        aa = num+num
 
-        print(aa)
+        aa = num*num*num + 5*num+6
+
+        bb = aa//6
+
+        print(bb)
+        
+
         
     except EOFError:
         break
