11133cc811a005da7916c97920439d3a7741e3de
Raymond <78069313+if-else-master@users.noreply.github.com>
2025-06-03 11:30:07 +0800

    update

--- b967.py
+++ b967.py
@@ -6,27 +6,29 @@ for i in range(num-1):
     fam = list(map(int,input().split()))
     family.append(fam)
 
-#print(len(family))
-
-big = []
-samil = []
-
-for j in range(len(family)-1):
-    if family[j][0] == 0:
-        aa = list(map(int,str(family[j][1])))
-        big.append(aa)        
-for k in range(len(big)):
-    for j in range(len(family)):
-        if family[j][0] == big[k][0]:            
-            big[k].append(family[j][1])
+print(len(family))
+ans = []
 
 
 
+for j in range(len(family)):
+    for k in range(len(family)):
+        if family[j][1] == family[k][0] and family[j][1] != 0:
+            print(family[k],family[j][1],family[k][0])
+            family[j].append(family[k][1])
+            ans.append(family[j])
+
+
+print(ans)
+cut = []
+
+for item in ans:
+    if item not in cut:
+        cut.append(item)
+
+print(cut)
 
 
 
-print(big)
-
-
-
-
+#print(family[0][1],family[4][0])
+#[[0, 1], [0, 2], [0, 3], [7, 0], [1, 4], [1, 5], [3, 6]]
