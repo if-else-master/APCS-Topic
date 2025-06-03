a526afb5cf7390bd22febc830bbd16a842af7f90
Raymond <78069313+if-else-master@users.noreply.github.com>
2025-06-03 14:12:50 +0800

    update

--- b967.py
+++ b967.py
@@ -2,32 +2,8 @@ num = int(input())
 
 family = []
 
-for i in range(num-1):
-    fam = list(map(int,input().split()))
+for i in range(num - 1):
+    fam = list(map(int, input().split()))
     family.append(fam)
 
-print(len(family))
-ans = []
-
-
-
-for j in range(len(family)):
-    for k in range(len(family)):
-        if family[j][1] == family[k][0] and family[j][1] != 0:           
-            family[j].append(family[k][1])
-            ans.append(family[j])
-
-
-print(ans)
-cut = []
-
-for item in ans:
-    if item not in cut:
-        cut.append(item)
-
-print(cut)
-
-
-
-#print(family[0][1],family[4][0])
-#[[0, 1], [0, 2], [0, 3], [7, 0], [1, 4], [1, 5], [3, 6]]
+print(family)
