f326db448cf09671e4338af7d2c0c1dc400687c7
Raymond <78069313+if-else-master@users.noreply.github.com>
2025-06-03 19:12:26 +0800

    update

--- b967.py
+++ b967.py
@@ -1,9 +1,37 @@
-num = int(input())
+grape = {}
 
-family = []
+fam = int(input())
 
-for i in range(num - 1):
-    fam = list(map(int, input().split()))
-    family.append(fam)
+def tree(grape,node, temp=None, depth=0):
+    if temp is None:
+        temp = set()
+    temp.add(node)
+    
+    far_code = node
+    max_depth = depth
 
-print(family)
+    for neigh in grape[node]:
+        if neigh not in temp:
+            next_node, next_depth = tree(grape,neigh,temp,depth+1)
+            if next_depth > max_depth:
+                far_code = next_node
+                max_depth = next_depth
+    return far_code, max_depth
+
+
+for _ in range(fam-1):
+    u, v = input().split()
+
+    if u not in grape:
+        grape[u] = []
+    if v not in grape:
+        grape[v] = []
+    grape[u].append(v)
+    grape[v].append(u)
+
+#print(list(grape.keys())[1])
+
+#由上往下計算深度
+u, _ = tree(grape,list(grape.keys())[0])
+_, din = tree(grape, u)
+print(din)
