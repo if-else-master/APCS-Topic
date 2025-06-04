3f4391c5d11a0e10acd52f92213cda56e1f82689
Raymond <78069313+if-else-master@users.noreply.github.com>
2025-06-04 12:36:49 +0800

    update

---
+++ c291.py
@@ -0,0 +1,34 @@
+a = int(input())
+
+num = list(map(int,input().split()))
+
+
+#0的小圈圈
+
+#i = 0
+#me[i] -> num[i] 0 -> 4
+#me[num[i]] -> num[me[num[i]]] 4 -> 6
+#me[num[me[num[i]]]] -> num[me[num[me[num[i]]]]] 6 -> 8
+
+
+def fr(num,i,stop,ans=0,temp=None):
+    if temp is None:
+        temp = []
+    ans = num[i]
+    temp.append(num[i])
+    if num[i] == 0:
+        return temp
+    else:
+        return fr(num,ans,ans,temp)
+    
+    
+k = 0
+
+for i in range(a):
+    aa = fr(num,0,num[i])
+    print(aa)
+
+print(k)
+
+    
+    
