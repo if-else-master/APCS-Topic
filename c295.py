b2b847ccd4b60cc412825eeaf6cc216228145b2f
Raymond <78069313+if-else-master@users.noreply.github.com>
2025-06-05 12:14:11 +0800

    update

--- c295.py
+++ c295.py
@@ -9,7 +9,23 @@ for i in range(N):
 for j in range(len(bb)):    
     num.append(bb[j])
 
+ans = []
+
+for k in range(len(num)):
+    ans.append(max(num[k]))
+    
 
 
+print(sum(ans))
+point = sum(ans)
+list_point = []
 
+for h in range(len(ans)):
+    if point % ans[h] == 0:
+        list_point.append(ans[h])
+
+if list_point == []:
+    print("-1")
+else:
+    print(' '.join(map(str,list_point)))
     
